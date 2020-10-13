28936a74013062e4cf13214a7d69312378f1a4df
dmoriley <david.m.oriley@hotmail.com>
2020-10-13 18:35:51 -0400

    Added correction functionality and button. Handling bad sytax errors and display message. Changed methods to useCallback methods and added to useEffect deps.

--- src/events/events.js
+++ src/events/events.js
@@ -2,5 +2,6 @@ export default {
   OPERAND_CLICKED: 'operand-clicked',
   OPERATOR_CLICKED: 'operator-clicked',
   CALCULATE: 'calculate',
-  CLEAR: 'clear'
+  CLEAR: 'clear',
+  CORRECTION: 'correction',
 }
